fd1a1b658793fbf5145e8f93924bbf4ebf95e59b
Jan Milewczyk <milewczykjan@gmail.com>
2026-06-14 20:26:50 +0200

    prisma script fix

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 echo "[entrypoint] Stosowanie migracji Prisma..."
-npx prisma migrate deploy
+node ./node_modules/prisma/build/index.js migrate deploy
 
 echo "[entrypoint] Uruchamianie Next.js..."
 exec node server.js
